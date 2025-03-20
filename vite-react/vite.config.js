f11bdcc3b862e4a7f806ab9bc0f1e5203a64f229
zaina1haq <zainaadnan11@gmail.com>
2025-03-20 22:04:26 +0200

    ئئئ

--- vite-react/vite.config.js
+++ vite-react/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      'react-icons/fa': 'react-icons/fa/index.js', // حل مشكلة استيراد react-icons
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['bootstrap', 'react-icons'], // ضمان عدم حصول مشاكل في الاستيراد
+    },
+  },
+  publicDir: 'public', // تأكد أن الصور داخل مجلد public سيتم قراءتها بشكل صحيح
+  assetsInclude: ['/.png', '/.jpg', '/.jpeg', '/.svg'], // دعم تحميل الصور
+});
